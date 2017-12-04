6d08e8ec1b9eecb6c8a87a6883ca8bac41260c1e
shawn <axe2781@gmail.com>
2017-12-04 14:08:48 -0500

    csss

--- jss/book.js
+++ jss/book.js
@@ -2,24 +2,25 @@ var artisInput = document.getElementById("artis"),
     lableInput = document.getElementById("lable"),
     addButton = document.getElementById("addButton"),
     displayList = document.getElementById("display-list"),
-    book = [];
+    books = [];
 
-var Book = function Book(artis, lable){
+var Book = function Book(artis, lable) {
     this.artis = artis;
     this.lable = lable;
-    this.displayList = function(){
+    this.getDisplayText = function () {
         return this.artis + " by " + this.lable;
     }
+}
 
-var renderList = function renderList(){
-        displayList.innerHTML = "";
-        books.forEach(function(book, index, arr){
-            displayList.innerHTML += book.getDisplayText() + "<br>";
-        });
-    };
- 
-    addButton.addEventListener("click", function(){
-        var newBook = new Book(artisInput.value, lableInput.value);
-        books.push(newBook);
-        renderList();
+var renderList = function renderList() {
+    displayList.innerHTML = "";
+    books.forEach(function (book, index, arr) {
+        displayList.innerHTML += book.getDisplayText() + "<br>";
     });
+};
+
+addButton.addEventListener("click", function () {
+    var newBook = new Book(artisInput.value, lableInput.value);
+    books.push(newBook);
+    renderList();
+});
